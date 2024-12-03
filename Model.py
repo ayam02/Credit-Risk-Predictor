4c02e8b64a5da9b9e2acc68299e68771ed299048
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-12-03 14:47:33 -0500

    fully functional form

--- Model.py
+++ Model.py
@@ -6,9 +6,12 @@ from sklearn.preprocessing import LabelEncoder
 
 class PredictiveModel:
     def __init__(self, filePath):
-        self.filePath(filePath)
+        self.filePath= filePath
         self.categorical_columns = ["checking_balance", 'credit_history', 'purpose', 'employment_duration', 'other_credit', 'housing', 'job', 'phone', 'savings_balance' ]
         self.encoders={}
+        self.trainingPreprocessing()
+        self.trainModel()
+
 
     def trainingPreprocessing(self):
         self.train_df = pd.read_csv(self.filePath)
@@ -18,7 +21,6 @@ class PredictiveModel:
             self.encoders[column] = label_encoder
 
     def trainModel(self):
-        self.preprocessData()
         X = self.train_df.drop("default", axis=1)
         Y = self.train_df["default"]
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -28,31 +30,32 @@ class PredictiveModel:
         # accuracy = accuracy_score(y_test, y_predicted)
         # print(accuracy)
 
-    def createInputDictionary(self, array_input):
-        self.input_dictionary = { "checking_balance":"> 200 DM",
-                    "months_loan_duration":10, 
-                    "credit_history":"good",
-                    "purpose":"furniture/appliances",
-                    "amount":1000,
-                    "savings_balance":"100 - 500 DM",
-                    "employment_duration": "unemployed",
-                    "percent_of_income":1,
-                    "years_at_residence":4,
-                    "age":30,
-                    "other_credit":"none",
-                    "housing":"own",
-                    "existing_loans_count":1,
-                    "job":"skilled",
-                    "dependents":2,
-                    "phone":"yes" }
+    # def createInputDictionary(self, array_input):
+    #     self.input_dictionary = { "checking_balance":"> 200 DM",
+    #                 "months_loan_duration":10, 
+    #                 "credit_history":"good",
+    #                 "purpose":"furniture/appliances",
+    #                 "amount":1000,
+    #                 "savings_balance":"100 - 500 DM",
+    #                 "employment_duration": "unemployed",
+    #                 "percent_of_income":1,
+    #                 "years_at_residence":4,
+    #                 "age":30,
+    #                 "other_credit":"none",
+    #                 "housing":"own",
+    #                 "existing_loans_count":1,
+    #                 "job":"skilled",
+    #                 "dependents":2,
+    #                 "phone":"yes" }
 
-    def testingPreprocessing (self):
+    def testingPreprocessing (self, input_dictionary):
         for category in self.categorical_columns:
-            user_input= self.input_dictionary.get(category)
-            self.input_dictionary[category] = self.encoders[category].transform([user_input])[0]
+            user_input= input_dictionary.get(category)
+            input_dictionary[category] = self.encoders[category].transform([user_input])[0]
         test_columns = self.train_df.columns.tolist()[:-1]
-        self.input_df = pd.DataFrame([self.input_dictionary.values()], columns=test_columns)
+        self.input_df = pd.DataFrame([input_dictionary.values()], columns=test_columns)
 
-    def testRisk (self):
+    def testRisk (self, input_dictionary):
+        self.testingPreprocessing(input_dictionary)
         default_probability = self.decision_tree.predict_proba(self.input_df)
-        return default_probability[0]
+        return default_probability
--- project.py
+++ project.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, request
+from Model import PredictiveModel
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    filePath = "credit.csv"
+    categories=["checking_balance", "months_loan_duration",	"credit_history",	"purpose","amount",	"savings_balance","employment_duration","percent_of_income","years_at_residence","age","other_credit","housing","existing_loans_count",	"job","dependents","phone"]
+     
+    
+
     # Check if the form is submitted (POST request)
     if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')   
-        random = "some random variable that i want to confirm can be accessed"     
-        return render_template("results.html", var = random)
+        input_dictionary = {}
+        for category in categories:
+            input_dictionary[category] = request.form.get(category)
 
+        #test the model -- find the probability 
+        default_probability = PredictiveModel(filePath).testRisk(input_dictionary)
+          
+        return render_template("results.html", default_risk = default_probability, dictionary= input_dictionary)
     # If it's a GET request, render the form
     return render_template('index.html')
 
