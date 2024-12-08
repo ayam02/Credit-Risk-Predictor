b5ebdcb378b9054f2b2bd4b099bcaced3b25fe05
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-12-08 16:14:08 -0500

    removed unused code

--- DataStats.py
+++ DataStats.py
@@ -1,16 +1,5 @@
 import pandas as pd
 import numpy as np
-# class DataStats:
-#     def __init__(self):
-#         self.filePath = "credit.csv"
-#         self.df = pd.read_csv(self.file_path)
-        #self.categorical_columns = ["checking_balance", "months_loan_duration", 'credit_history', 'purpose', "savings_balance", 'employment_duration', 'other_credit', 'housing', 'job', 'phone' ]
-
-    # def uniqueValues(self):
-    #     df = pd.read_csv('credit.csv')
-    #     for col in self.categorical_columns:
-    #         print(col, " has ", df[col].nunique(), " unique values ")
-    #         print(df[col].unique())
 
 class DataStats:
     #Define/separate numerical and categorical columns (so i don't get a typeerror)
