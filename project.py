4eb255152522ef6702bd4df45c4e96d2525f8fd6
aya-m1 <83889617+ayam02@users.noreply.github.com>
2024-12-08 16:13:56 -0500

    added categories to project.py

--- project.py
+++ project.py
@@ -6,8 +6,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     filePath = "credit.csv"
-    categories=["checking_balance", "months_loan_duration",	"credit_history",	"purpose","amount",	"savings_balance","employment_duration","percent_of_income","years_at_residence","age","other_credit","housing","existing_loans_count",	"job","dependents","phone"]
+
+    categories=[
+        "checking_balance", "months_loan_duration",	"credit_history",
+        "purpose","amount",	"savings_balance","employment_duration",
+        "percent_of_income","years_at_residence","age","other_credit",
+        "housing","existing_loans_count",	"job","dependents","phone"]
+    
+    numerical_columns = [
+        'months_loan_duration', 'amount', 
+        'percent_of_income', 'years_at_residence', 
+        'age', 'existing_loans_count', 'dependents'
+    ]
+    
+    categorical_columns = ['checking_balance', 'credit_history', 'purpose', 
+                           'savings_balance', 'employment_duration',
+                           'other_credit', 'housing', 'job', 'phone']
+
     # Check if the form is submitted (POST request)
     if request.method == 'POST':
         input_dictionary = {}
@@ -16,18 +33,21 @@ def index():
 
         #test the model -- find the probability 
         default_probability = PredictiveModel(filePath).testRisk(input_dictionary)
+
         #Generate charts with user comparison
-        charts = ChartGenerator().generate_charts(user_input=input_dictionary)
+        charts = ChartGenerator(filePath).generate_charts(user_input=input_dictionary)
+
         profile = {
             "savings_balance": request.form.get('savings_balance', 0, type=int),
             "employment_duration": request.form.get('employment_duration', 'unemployed')
         }
+   
         
         #Get all insights
         insights = DataStats().generate_all_insights(profile)
 
         #Pass insights to the results page
-        return render_template("results.html", default_risk=default_probability, charts=charts, insights=insights)
+        return render_template("results.html", numerical_columns= numerical_columns, categorical_columns = categorical_columns, default_risk=default_probability, charts=charts, insights=insights)
           
         #return render_template("results.html", default_risk = default_probability, dictionary= input_dictionary)
     # If it's a GET request, render the form
